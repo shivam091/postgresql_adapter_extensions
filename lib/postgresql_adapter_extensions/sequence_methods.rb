ba2d6643651f5210a436a3b15731437d5fa617e9
Harshal LADHE <harshal.ladhe.1@gmail.com>
2025-03-12 19:33:53 +0530

    docs: updated rdocs

--- lib/postgresql_adapter_extensions/sequence_methods.rb
+++ lib/postgresql_adapter_extensions/sequence_methods.rb
@@ -21,12 +21,12 @@ module PostgreSQLAdapterExtensions
     #
     # @param name [String, Symbol] The name of the sequence to create.
     # @param options [Hash] Additional options to configure the sequence.
-    # @option options [Boolean] :if_not_exists (false) Includes `IF NOT EXISTS` to avoid errors if the sequence exists.
-    # @option options [String, nil] :data_type (nil) Sets the sequence's data type (e.g., `BIGINT`, `SMALLINT`).
+    # @option options [Boolean] :if_not_exists (false) Includes +IF NOT EXISTS+ to avoid errors if the sequence exists.
+    # @option options [String, nil] :data_type (nil) Sets the sequence's data type (e.g., +BIGINT+, +SMALLINT+).
     # @option options [Integer] :start (1) The starting value of the sequence.
     # @option options [Integer] :increment_by (1) The increment step for each sequence value.
-    # @option options [Integer, nil] :minvalue (1) The minimum value the sequence can generate. Uses `NO MINVALUE` if nil.
-    # @option options [Integer, nil] :maxvalue (nil) The maximum value the sequence can generate. Uses `NO MAXVALUE` if nil.
+    # @option options [Integer, nil] :minvalue (1) The minimum value the sequence can generate. Uses +NO MINVALUE+ if nil.
+    # @option options [Integer, nil] :maxvalue (nil) The maximum value the sequence can generate. Uses +NO MAXVALUE+ if nil.
     # @option options [Integer] :cache (1) The number of sequence values to cache for performance.
     # @option options [Boolean] :cycle (false) Whether the sequence should cycle back to the start after reaching the max value.
     # @option options [String, nil] :owned_by (nil) The table and column name that owns this sequence.
@@ -67,7 +67,7 @@ module PostgreSQLAdapterExtensions
       sql << (options[:maxvalue] ? " MAXVALUE #{options[:maxvalue]}" : " NO MAXVALUE")
       sql << " START WITH #{options[:start]}" if options[:start]
       sql << " CACHE #{options[:cache]}" if options[:cache]
-      sql << (options[:cycle] ? " CYCLE" : " NO CYCLE")
+      sql << " #{options[:cycle] ? 'CYCLE' : 'NO CYCLE'}"
       sql << " OWNED BY #{options[:owned_by]}" if options[:owned_by]
 
       execute(sql).tap { reload_type_map }
@@ -80,16 +80,16 @@ module PostgreSQLAdapterExtensions
     #
     # @param name [String, Symbol] The name of the sequence to alter.
     # @param options [Hash] A hash of options to modify the sequence behavior.
-    # @option options [Boolean] :if_exists Includes `IF EXISTS` to avoid errors if the sequence does not exist.
-    # @option options [String, nil] :data_type Sets the sequence's data type (e.g., `BIGINT`, `SMALLINT`).
+    # @option options [Boolean] :if_exists Includes +IF EXISTS+ to avoid errors if the sequence does not exist.
+    # @option options [String, nil] :data_type Sets the sequence's data type (e.g., +BIGINT+, +SMALLINT+).
     # @option options [Integer] :increment_by Sets the increment step for the sequence.
-    # @option options [Integer, nil] :minvalue Sets the minimum value for the sequence. Uses `NO MINVALUE` if nil.
-    # @option options [Integer, nil] :maxvalue Sets the maximum value for the sequence. Uses `NO MAXVALUE` if nil.
+    # @option options [Integer, nil] :minvalue Sets the minimum value for the sequence. Uses +NO MINVALUE+ if nil.
+    # @option options [Integer, nil] :maxvalue Sets the maximum value for the sequence. Uses +NO MAXVALUE+ if nil.
     # @option options [Integer] :start Sets the starting value of the sequence.
-    # @option options [Integer, nil] :restart Restarts the sequence. Uses `RESTART` if nil, `RESTART WITH value` if provided.
+    # @option options [Integer, nil] :restart Restarts the sequence. Uses +RESTART+ if nil, +RESTART WITH value+ if provided.
     # @option options [Integer] :cache Sets the number of sequence values to cache for performance.
-    # @option options [Boolean] :cycle Enables (`CYCLE`) or disables (`NO CYCLE`) sequence cycling.
-    # @option options [String, nil] :owned_by Associates the sequence with a table column. Uses `OWNED BY NONE` if nil.
+    # @option options [Boolean] :cycle Enables (+CYCLE+) or disables (+NO CYCLE+) sequence cycling.
+    # @option options [String, nil] :owned_by Associates the sequence with a table column. Uses +OWNED BY NONE+ if nil.
     #
     # @example Modify the increment value of a sequence
     #   alter_sequence(:order_id_seq, increment_by: 10)
@@ -126,7 +126,7 @@ module PostgreSQLAdapterExtensions
       sql << " RESTART" if options[:restart].nil? && options.key?(:restart)
       sql << " RESTART WITH #{options[:restart]}" if options[:restart]
       sql << " CACHE #{options[:cache]}" if options[:cache]
-      sql << (options[:cycle] ? " CYCLE" : " NO CYCLE")
+      sql << " #{options[:cycle] ? 'CYCLE' : 'NO CYCLE'}"
       sql << (options[:owned_by] ? " OWNED BY #{options[:owned_by]}" : " OWNED BY NONE")
 
       execute(sql).tap { reload_type_map }
@@ -140,10 +140,10 @@ module PostgreSQLAdapterExtensions
     #
     # @param name [String, Symbol] The name of the sequence to drop.
     # @param options [Hash] Additional options to modify the behavior of the drop operation.
-    # @option options [Boolean] :if_exists (false) Adds `IF EXISTS` to avoid errors if the sequence does not exist.
-    # @option options [Symbol] :drop_behavior (`nil`) Determines whether dependent objects are also dropped.
-    #   - Accepts `:cascade` to drop dependent objects.
-    #   - Accepts `:restrict` to prevent dropping if dependencies exist.
+    # @option options [Boolean] :if_exists (false) Adds +IF EXISTS+ to avoid errors if the sequence does not exist.
+    # @option options [Symbol] :drop_behavior (nil) Determines whether dependent objects are also dropped.
+    #   - Accepts +:cascade+ to drop dependent objects.
+    #   - Accepts +:restrict+ to prevent dropping if dependencies exist.
     #
     # @example Drop a sequence without additional options
     #   drop_sequence(:order_id_seq)
